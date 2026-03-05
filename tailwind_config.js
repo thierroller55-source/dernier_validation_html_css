498c41b307977a421fd7f6636d8f918ad6c3da6f
thierroller55-source <thierroller55@gmail.com>
2026-03-05 15:39:26 +0000

    drenier tache html css

---
+++ tailwind_config.js
@@ -0,0 +1,63 @@
+ 
+        tailwind.config = {
+            theme: {
+                //screens:{
+                    //tablet:"600px",
+               // },
+                extend: {
+                    colors: {
+                        vert: '#32a852',
+                        blanc: '#f7faf8',
+                        rouge: '#d10210',
+                        meuve: '#8902d1',
+                        rose:"#fc72e1",
+                        blanc: '#faf7f8', 
+                        bleu: '#0b1bd4',
+                        black:'#0a0a0a',
+                        zone:'#d9d507',
+                        orange:'#d95707', 
+                        siro:"#0cd2e8", 
+                        orange_fnc:"#FF9900",
+                        vert_mt:"#009E86",
+                        orange_clair:"#f3e5cd",
+                        bleu_clair:"#ddeef0", 
+                        vert_clair:"#d9f0ec",
+                        rose_clair:"#f5e5f2",                
+                    },
+                     backgroundImage: {
+                        profil: "url('images_tlwd/profil-0.png')",
+                    },
+                    boxShadow: {
+                        bs:"2px 10px 10px 8px rgba(15, 15, 15,0.5)",
+                    },
+                    borderWidth:{
+                        bw:"8px",
+                    },
+                    borderColor:{
+                        bc:"#d4830b",
+                        bl:"#344ceb",
+                        siro:"#0cd2e8",
+                    },
+                    borderRadius: {
+                        br:"20px",
+                        brfl:"9999px",
+                    },
+                      margin:{
+                        mg:"40px",
+                    },
+                      padding:{
+                        pdg:"40px",
+                    },
+                     height:{
+                        ht:"350px",
+                    },
+                      width:{
+                        wd:"40%",
+                    },
+                      gap:{
+                        gp:"16",
+                    },
+                }
+            }
+        }
+    
